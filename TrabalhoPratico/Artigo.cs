4f1af8847346d17e4e8453273480e982eb7a1232
Unknown <j.lucas12@hotmail.com>
2017-01-05 11:23:57 +0000

    Class Artigo

---
+++ TrabalhoPratico/Artigo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_pratico
+{
+    public class Artigo
+    {
+        private string codigocartao;
+        public string Codigocartao
+        {
+            get { return codigocartao; }
+            set { codigocartao = value; }
+        }
+
+        private string descricao;
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        private float precounitario;
+        public float Precounitario
+        {
+            get { return precounitario; }
+            set { precounitario = value; }
+        }
+
+        private int quantidadestock;
+        public int Quantidadestock
+        {
+            get { return quantidadestock; }
+            set { quantidadestock = value; }
+        }
+
+        public Artigo(string codigo, string descricao, float precounitario, int quantidadestock)
+        {
+            this.Codigocartao = codigo;
+            this.Descricao = descricao;
+            this.Precounitario = precounitario;
+            this.Quantidadestock = quantidadestock;
+        }
+    }
+}
+
