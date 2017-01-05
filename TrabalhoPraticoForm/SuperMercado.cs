dc4cff2d2deddc9de7f18a371a9385510f33b156
Unknown <FernandoC.9@hotmail.com>
2017-01-05 15:44:11 +0000

    Classe SuperMercado

---
+++ TrabalhoPraticoForm/SuperMercado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabalho_pratico;
+using TrabalhoPratico;
+
+namespace TrabalhoPraticoForm
+{
+    class SuperMercado
+    {
+        private string nome;
+        private Dictionary<string, Artigo> ListaArtigos;
+        private List<Cartao> ListaClientes;
+    }
+}
