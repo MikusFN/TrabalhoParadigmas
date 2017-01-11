6a4ad5e81787c405f561b937bbde4c0034b28926
Unknown <FernandoC.9@hotmail.com>
2017-01-11 14:36:51 +0000

    Classe Supermeercado completa

--- TrabalhoPraticoForm/SuperMercado.cs
+++ TrabalhoPraticoForm/SuperMercado.cs
@@ -11,7 +11,20 @@ namespace TrabalhoPraticoForm
     class SuperMercado
     {
         private string nome;
-        private Dictionary<string, Artigo> ListaArtigos;
-        private List<Cartao> ListaClientes;
+        private Dictionary<string, Artigo> listaartigos;
+        private List<Cartao> listaclientes;
+        public string Nome { get; set; }
+        public  Dictionary<string, Artigo> ListaArtigos { get; set; }
+        public List<Cartao> ListaClientes { get; set; }
+
+        public SuperMercado(string nome)
+        {
+            this.nome = nome;
+            this.listaartigos = new Dictionary<string, Artigo>();
+            this.listaclientes = new List<Cartao>();
+        }
+
+
+
     }
 }
